e7d67995da64e41f6fb5aeb43df725d3dadb7f30
guyjof <guyjofre02@gmail.com>
2021-11-17 00:21:28 +0200

    menu fix

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  close: boolean = true
+
+  closeMenu() {
+    this.close = false
+  }
 }
--- client/src/app/components/app-header/app-header.component.ts
+++ client/src/app/components/app-header/app-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-app-header',
@@ -6,16 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-header.component.scss']
 })
 export class AppHeaderComponent implements OnInit {
-
+  @Input() close: boolean = true
   menuIsOpen: boolean = false
-
-  toggleMenu() {
-    this.menuIsOpen = !this.menuIsOpen
-  }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
+    this.menuIsOpen = this.close
+  }
+
+  toggleMenu() {
+    this.menuIsOpen = !this.menuIsOpen
   }
 
 }
